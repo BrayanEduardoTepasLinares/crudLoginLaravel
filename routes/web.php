b8b1b18fef76e1badeb5e99d45c230c907ff517a
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-08-07 14:00:44 -0600

    se ejecuto el comando para crear los crud y se añadio a las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('rols', RolController::class);
+Route::resource('users', UserController::class);
